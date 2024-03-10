6c66071dd781091bd523812a44a725e005e12dcd
Juliano Sill <sill.juliano@gmail.com>
2024-03-09 21:24:50 -0300

    create first post route to test

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
-import type { Config } from "drizzle-kit"
-import { env } from "./src/env";
+import type { Config } from 'drizzle-kit'
+
+import { env } from './src/env'
 
 export default {
-  schema: "./src/db/schema/index.ts",
-  out: "./drizzle",
-  driver: "pg",
+  schema: './src/db/schema/index.ts',
+  out: './drizzle',
+  driver: 'pg',
   dbCredentials: {
     connectionString: env.DATABASE_URL,
-  }
-} satisfies Config;
+  },
+} satisfies Config
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,34 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello World'
-})
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
+
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body
+
+    const [manager] = await db
+      .insert(users)
+      .values({ name: managerName, email, phone, role: 'manager' })
+      .returning({ id: users.id })
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager.id,
+    })
+
+    set.status = 204
+  },
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      managerName: t.String(),
+      email: t.String({ format: 'email' }),
+      phone: t.String(),
+    }),
+  },
+)
 
 app.listen(3333, () => {
   console.log('HTTP server running')
