1b29f2917fc4473264fcabd42c18ebe0f6df2297
Juliano Sill <sill.juliano@gmail.com>
2024-03-12 21:52:06 -0300

    feat(route): create get-profile

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,7 @@
 import { Elysia } from 'elysia'
 
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getProfile } from './routes/get-profile'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 import { signOut } from './routes/sign-out'
@@ -10,6 +11,7 @@ const app = new Elysia()
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(signOut)
+  .use(getProfile)
 
 app.listen(3333, () => {
   console.log('HTTP server running')
