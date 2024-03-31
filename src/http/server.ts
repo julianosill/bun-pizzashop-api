83050a9154f70d926c357589c338a4f9eb615698
Juliano Sill <sill.juliano@gmail.com>
2024-03-30 23:49:14 -0300

    chore: add cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,14 +1,16 @@
+import { cors } from '@elysiajs/cors'
 import { Elysia } from 'elysia'
 
+import { env } from '../env'
 import { approveOrder } from './routes/approve-order'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { cancelOrder } from './routes/cancel-order'
 import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
-import { getCanceledMonthOrdersAmount } from './routes/get-canceled-month-orders-amount'
 import { getDailyRevenueInPeriod } from './routes/get-daily-revenue-in-period'
 import { getDayOrdersAmount } from './routes/get-day-orders-amount'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
+import { getMonthCanceledOrdersAmount } from './routes/get-month-canceled-orders-amount'
 import { getMonthOrdersAmount } from './routes/get-month-orders-amount'
 import { getMonthRevenue } from './routes/get-month-revenue'
 import { getOrderDetails } from './routes/get-order-details'
@@ -20,6 +22,22 @@ import { sendAuthLink } from './routes/send-auth-link'
 import { signOut } from './routes/sign-out'
 
 const app = new Elysia()
+  .use(
+    cors({
+      credentials: true,
+      allowedHeaders: ['content-type'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+      origin: (request): boolean => {
+        const origin = request.headers.get('origin')
+
+        if (!origin || origin !== env.AUTH_REDIRECT_URL) {
+          return false
+        }
+
+        return true
+      },
+    }),
+  )
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
@@ -35,7 +53,7 @@ const app = new Elysia()
   .use(getMonthRevenue)
   .use(getDayOrdersAmount)
   .use(getMonthOrdersAmount)
-  .use(getCanceledMonthOrdersAmount)
+  .use(getMonthCanceledOrdersAmount)
   .use(getPopularProducts)
   .use(getDailyRevenueInPeriod)
   .onError(({ code, error, set }) => {
